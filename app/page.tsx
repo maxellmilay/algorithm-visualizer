f25484bc725da37d1e477da1903ed4fd20b97d48
Maxell Milay <milaymaxell@gmail.com>
2024-05-17 23:19:17 +0800

    [Feature][Maxell] setup tree dsa

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Tree from '../components/structures/Tree'
 
 export default function Page() {
     return (
       <div className='flex flex-col w-full'>
         <h1 className='text-center'>Tree</h1>
+        <Tree/>
       </div>
     )
   }
---
+++ components/structures/Tree.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { useRef, useEffect } from 'react'
+import * as d3 from "d3"
+
+const Tree = () => {
+    return (
+    <div>Tree DSA</div>
+    )
+}
+
+export default Tree
